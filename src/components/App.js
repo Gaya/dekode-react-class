a6b0817d674208f2fb2cd5ffa06cb799be7f5613
Gaya Kessler <gaya.kessler@gmail.com>
2018-09-03 13:59:19 +0200

    add assignment code

--- src/components/App.js
+++ src/components/App.js
@@ -9,9 +9,40 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Welcome to Dekode React Class!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="App-container">
+          <div className="App-assignments">
+            <h2>First assignment:</h2>
+            <p>
+              Display the list of questions creating two new components: QuestionsList, Question.
+            </p>
+
+            <h2>Second assignment:</h2>
+            <p>
+              Create a form with input which communicates adding a question through the App component.
+              (1. App passes callback to form, 2. form calls callback with new question, 3. App adds question to state)
+            </p>
+
+            <h2>Third assignment (BONUS!):</h2>
+            <p>
+              Make some UX improvements:
+              <ul>
+                <li>
+                  Clear question input when adding.
+                </li>
+                <li>
+                  Press "return" to add question.
+                </li>
+                <li>
+                  Remove questions.
+                </li>
+                <li>
+                  Edit questions.
+                </li>
+              </ul>
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
